8846ad34b3a0721b1da57355749c7a5b1e9ffea9
Gabriel Batista da Silva <gabriel.batista@take.net>
2024-07-18 08:35:19 -0300

    Add project files.

---
+++ StringCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            var num = numbers;
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+            if(numbers.StartsWith("//"))
+            {
+               
+               ///...
+            }
+            num = num.Replace('\n',',');
+            return num.Split(',').Select(n => int.Parse(n)).Sum();
+        }
+    }
+}
